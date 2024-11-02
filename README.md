52ff415b77ed1d005d1d8363eb7c556e92833447
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2024-11-02 22:00:29 +0100

    feat: add periodic boundry conditions

---
+++ README.md
@@ -0,0 +1,48 @@
+# wasm-elementary-cellular-automaton
+
+A simple elementary cellular automaton implemented in Rust and compiled to WebAssembly.
+
+## Adding to JS/TS project
+
+- Add to `package.json`:
+
+```bash
+npm install wasm-elementary-cellular-automaton
+```
+
+You can use bun, pnpm, yarn, deno or any other package manager.
+
+- Import in your JS/TS code:
+
+```typescript
+import { CellularAutomaton } from "wasm-elementary-cellular-automaton";
+```
+
+- Use it:
+
+Example with React and TailwindCSS:
+```typescript
+const App = () => {
+    const initialState = new Uint8Array(128).fill(0);
+    initialState[0] = 1;
+    
+    const automaton = new CellularAutomaton(128, initialState, false);
+    const [vector, setVector] = useState(automaton.get_state());
+    useEffect(() => {
+        const interval = setInterval(() => {
+            automaton.step();
+            setVector(automaton.get_state());
+        }, 100);
+        return () => clearInterval(interval);
+    }, []);
+    return (
+        <div>
+            {vector.map((cell, index) => <div key={index} className={cell ? 'bg-black' : 'bg-white' + " w-4 h-4"} />)}
+        </div>
+    )
+}
+```
+
+Full example you can find here: https://github.com/wleklinskimateusz/cellular-automaton-react
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,17 @@ pub struct CellularAutomaton {
     initial_state: u128,
 }
 
+fn vec_to_number(vec: Vec<u8>) -> u128 {
+    vec.iter().fold(0, |acc, &bit| (acc << 1) | bit as u128)
+}
+
 #[wasm_bindgen]
 impl CellularAutomaton {
-    pub fn new(rule: u8, initial_state: Vec<u8>) -> CellularAutomaton {
-        let to_number: u128 = initial_state
-            .iter()
-            .fold(0, |acc, &bit| (acc << 1) | bit as u128);
+    pub fn new(rule: u8, initial_state: Vec<u8>, periodic_boundary: bool) -> CellularAutomaton {
+        let initial_state_number = vec_to_number(initial_state);
         CellularAutomaton {
-            automaton: Automaton::new(rule, to_number),
-            initial_state: to_number,
+            automaton: Automaton::new(rule, initial_state_number, periodic_boundary),
+            initial_state: initial_state_number,
         }
     }
 
@@ -24,11 +26,16 @@ impl CellularAutomaton {
     }
 
     pub fn get_state(&self) -> Vec<u8> {
-        self.automaton.to_vector()
+        self.automaton.to_list().to_vec()
     }
 
     pub fn set_rule(&mut self, rule: u8) {
         self.automaton.rule = rule;
+    }
+
+    pub fn set_initial_state(&mut self, initial_state: Vec<u8>) {
+        self.initial_state = vec_to_number(initial_state);
+        self.automaton.fields = self.initial_state;
     }
 
     pub fn reset(&mut self) {
@@ -44,20 +51,20 @@ mod tests {
 
     #[test]
     fn test_new() {
-        let automaton = CellularAutomaton::new(30, vec![1; 128]);
+        let automaton = CellularAutomaton::new(30, vec![1; 128], false);
         assert_eq!(automaton.automaton.rule, 30);
     }
 
     #[test]
     fn test_step_rule_0() {
-        let mut automaton = CellularAutomaton::new(0, vec![1; 128]);
+        let mut automaton = CellularAutomaton::new(0, vec![1; 128], false);
         automaton.step();
         assert_eq!(automaton.get_state(), vec![0; 128]);
     }
 
     #[test]
     fn test_step_rule_30() {
-        let mut automaton = CellularAutomaton::new(30, vec![1; 128]);
+        let mut automaton = CellularAutomaton::new(30, vec![1; 128], false);
         automaton.step();
         // For rule 30, when all cells are 1, the next state will be all 0s except the left edge
         let mut expected = vec![0; 128];
@@ -67,7 +74,7 @@ mod tests {
 
     #[test]
     fn test_reset() {
-        let mut automaton = CellularAutomaton::new(30, vec![1; 128]);
+        let mut automaton = CellularAutomaton::new(30, vec![1; 128], false);
         automaton.step();
         automaton.reset();
         assert_eq!(automaton.get_state(), vec![1; 128]);
@@ -75,8 +82,31 @@ mod tests {
 
     #[test]
     fn test_set_rule() {
-        let mut automaton = CellularAutomaton::new(30, vec![1; 128]);
+        let mut automaton = CellularAutomaton::new(30, vec![1; 128], false);
         automaton.set_rule(0);
         assert_eq!(automaton.automaton.rule, 0);
+    }
+
+    #[test]
+    fn test_set_initial_state() {
+        let mut automaton = CellularAutomaton::new(30, vec![1; 128], false);
+        automaton.set_initial_state(vec![0; 128]);
+        assert_eq!(automaton.initial_state, 0);
+        assert_eq!(automaton.get_state(), vec![0; 128]);
+    }
+
+    #[test]
+    fn test_periodic_boundary() {
+        let mut automaton = CellularAutomaton::new(128, vec![1; 128], false);
+        automaton.step();
+        let mut expected = vec![1; 128];
+        expected[0] = 0;
+        expected[127] = 0;
+
+        assert_eq!(automaton.get_state(), expected);
+
+        let mut automaton = CellularAutomaton::new(128, vec![1; 128], true);
+        automaton.step();
+        assert_eq!(automaton.get_state(), vec![1; 128]);
     }
 }
